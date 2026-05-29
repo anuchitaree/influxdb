3e1f7a55275b32b59401bc1ce901b4cc33c72ebc
anuchitaree <86705763+anuchitaree@users.noreply.github.com>
2026-05-29 14:31:36 +0700

    add code

--- messagequque/Data/AppDbContext.cs
+++ messagequque/Data/AppDbContext.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Microsoft.EntityFrameworkCore;  
 namespace messagequque.Data
 {
-    internal class AppDbContext
-    {
-    }
-}
+    public  class AppDbContext: DbContext 
+       public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }  
+  
+    public DbSet<MessageEntity> Messages => Set<MessageEntity>();  
+  
+    protected override void OnModelCreating(ModelBuilder modelBuilder)  
+    {  
+        modelBuilder.Entity<MessageEntity>()  
+            .HasIndex(x => x.MessageId)  
+            .IsUnique();  
+    }  
+}  
