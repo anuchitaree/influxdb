3e1e49bac8bca6f187a1aff584209eddbe196674
diatanuchit <anuchit.aree.a9f@ap.denso.com>
2026-05-29 14:23:17 +0700

    add messagequque

---
+++ messagequque/Data/MessageEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace messagequque.Data
+{
+    public class MessageEntity
+    {
+        public long Id { get; set; }
+        public string MessageId { get; set; } = Guid.NewGuid().ToString();
+        public string Topic { get; set; } = string.Empty;
+        public string Payload { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public string Status { get; set; } = "pending";
+        public int RetryCount { get; set; } = 0;
+    }
+}
